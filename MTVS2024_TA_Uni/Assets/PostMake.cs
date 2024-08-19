34327b3dbb52e94d6783cdeabcacdb77f86ffef2
mcnail <mc.nail84@gmail.com>
2024-08-19 15:38:42 +0900

    UV distortion

--- MTVS2024_TA_Uni/Assets/PostMake.cs
+++ MTVS2024_TA_Uni/Assets/PostMake.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
 public class PostMake : MonoBehaviour
 {
-    Bloom bloom;
+    public VolumeProfile VolumeProfile;
+    public float bloomVal;
+    Bloom bm;
+
+    float time = 0;
     // Start is called before the first frame update
     void Start()
     {
+        VolumeProfile.TryGet(out bm);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        time += Time.deltaTime; 
+        bm.intensity.value = bloomVal;
     }
 }
